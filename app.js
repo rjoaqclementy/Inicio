6f8ecee5a3e994106bf4d3b6c7468e13dd3f790a
rjoaqclementy <ronald.arjcy@gmail.com>
2022-09-21 12:46:58 -0500

    mi primer commit

---
+++ app.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    let titulo = document.getElementById("titulo")
+
+    /* var variable2
+    let variable1
+    const variable3 */
+
+    titulo.style.fontSize = "30px"
+    titulo.style.color = "green"
+    titulo.style.marginLeft = "100px"
+    /* titulo.style.transform() */
+}
